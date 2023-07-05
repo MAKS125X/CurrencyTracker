1cb7d30246191dde658f1ce9588c64832630f810
MaksimShchelokov <MAKS125X@yandex.ru>
2023-07-05 11:17:36 +0400

    Created recyclerView in MainActivity with basic internet Request to API

--- app/src/main/java/com/example/currencytracker/MainActivity.kt
+++ app/src/main/java/com/example/currencytracker/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.currencytracker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.currencytracker.api.RetrofitInstance
+import com.example.currencytracker.databinding.ActivityMainBinding
+import com.example.currencytracker.models.toCurrencyModelList
+import com.example.currencytracker.ui.FavouriteCurrencyAdapter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var currencyAdapter: FavouriteCurrencyAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val list = RetrofitInstance.apiService.getMyData()
+            Log.d("Aboba", list.toString())
+            currencyAdapter = FavouriteCurrencyAdapter(list.toCurrencyModelList())
+            withContext(Dispatchers.Main) {
+
+                binding.recyclerView.layoutManager = LinearLayoutManager(applicationContext)
+
+
+                binding.recyclerView.adapter = currencyAdapter
+
+                currencyAdapter.notifyDataSetChanged()
+            }
+        }
+
+
     }
 }
