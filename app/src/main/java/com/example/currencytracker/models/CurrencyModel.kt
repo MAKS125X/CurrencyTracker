12f4490b007a6c85f16708ae9af074a1d9870c77
MaksimShchelokov <MAKS125X@yandex.ru>
2023-07-05 11:25:42 +0400

    Created currency's basic model for displaying in recyclerView

---
+++ app/src/main/java/com/example/currencytracker/models/CurrencyModel.kt
@@ -0,0 +1,16 @@
+package com.example.currencytracker.models
+
+data class CurrencyModel(
+    val ID: String,
+    val NumCode: String,
+    val CharCode: String,
+    val Nominal: Int,
+    val Name: String,
+    val Value: Double,
+    val Previous: Double,
+    val Date: String
+) {
+    fun getDifference() = this.Value - this.Previous
+    fun getSign() = if (getDifference() > 0) "+" else "-"
+    fun getPercentageChange() = (getDifference() / this.Value) * 100
+}
