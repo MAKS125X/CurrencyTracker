f19ddd7b4fea804549c7853324da1bfab6d39feb
MaksimShchelokov <MAKS125X@yandex.ru>
2023-07-05 11:23:12 +0400

    Created basic recyclerView Adapter for list of currencies

---
+++ app/src/main/java/com/example/currencytracker/ui/FavouriteCurrencyAdapter.kt
@@ -0,0 +1,69 @@
+package com.example.currencytracker.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.green
+import androidx.recyclerview.widget.RecyclerView
+import com.example.currencytracker.R
+import com.example.currencytracker.databinding.ItemCurrencyBinding
+import com.example.currencytracker.models.CurrencyModel
+import com.example.currencytracker.models.format
+import kotlin.math.absoluteValue
+
+class FavouriteCurrencyAdapter(private val dataSet: MutableList<CurrencyModel>) :
+    RecyclerView.Adapter<FavouriteCurrencyAdapter.CurrencyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CurrencyViewHolder =
+        CurrencyViewHolder(ItemCurrencyBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+
+    override fun onBindViewHolder(holder: CurrencyViewHolder, position: Int) {
+        holder.bind(dataSet[position])
+    }
+
+    override fun getItemCount() = dataSet.size
+
+    class CurrencyViewHolder(_binding: ItemCurrencyBinding) : RecyclerView.ViewHolder(_binding.root) {
+
+        var binding = ItemCurrencyBinding.bind(itemView)
+
+        fun bind(model: CurrencyModel) {
+            with(binding) {
+                imageView.setImageResource(R.drawable.ic_usd)
+                currencyName.text = model.Name
+                date.text = model.Date.subSequence(0,9)
+                nominalWithCode.text = "${model.Nominal} ${model.CharCode}"
+                currentCost.text = model.Value.toString()
+                costChangeValue.text =
+                    "${model.getSign()}${model.getDifference().absoluteValue.format(3)} / " +
+                            "${model.getSign()}${model.getPercentageChange().format(1)}%"
+
+                if (model.Value - model.Previous > 0) {
+                    costChangeMark.setImageResource(R.drawable.arrow_up)
+                    currentCost.setTextColor(
+                        ContextCompat.getColor(
+                            binding.currentCost.context,
+                            R.color.red
+                        )
+                    )
+                    costChangeValue.setTextColor(
+                        ContextCompat.getColor(
+                            binding.costChangeValue.context,
+                            R.color.red
+                        )
+                    )
+                } else {
+                    costChangeMark.setImageResource(R.drawable.arrow_down)
+                    currentCost.setTextColor(android.R.attr.color.green)
+                    costChangeValue.setTextColor(android.R.attr.color.green)
+                }
+
+                costChangeMark.setImageResource(
+                    if (model.Value - model.Previous > 0)
+                        R.drawable.arrow_up else R.drawable.arrow_down
+                )
+            }
+        }
+    }
+}
