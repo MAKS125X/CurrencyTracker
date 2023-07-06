cf21418e3c291b4c36d99706d0fc52fe0e8d5dfd
MaksimShchelokov <MAKS125X@yandex.ru>
2023-07-06 18:35:42 +0400

    Optimized imports

--- app/src/main/java/com/example/currencytracker/repository/ResponseRepository.kt
+++ app/src/main/java/com/example/currencytracker/repository/ResponseRepository.kt
@@ -1,10 +1,20 @@
 package com.example.currencytracker.repository
 
+import android.util.Log
+import com.example.currencytracker.api.ResponseNetwork
 import com.example.currencytracker.api.RetrofitInstance
 
 
 object ResponseRepository {
-    suspend fun getCurrencyList(): com.example.currencytracker.api.Response {
-        return RetrofitInstance.apiService.getMyData()
+
+    //получение данных из сети или из бд
+    suspend fun getCurrencyList(): ResponseNetwork {
+        val aboba = RetrofitInstance.apiService.getMyData()
+        for (item in aboba.currency){
+            Log.d("Aboba1", item.toString())
+        }
+        return aboba
     }
+
+
 }
