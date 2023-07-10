8c126910473858a3a2f8ae8d2f1b45f0bdaa3aec
MaksimShchelokov <MAKS125X@yandex.ru>
2023-07-10 16:02:52 +0400

    Dao for CRUD currency entity

--- app/src/main/java/com/example/currencytracker/db/CurrencyDao.kt
+++ app/src/main/java/com/example/currencytracker/db/CurrencyDao.kt
@@ -1,4 +1,20 @@
 package com.example.currencytracker.db
 
-class CurrencyDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.currencytracker.repository.Currency
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CurrencyDao {
+    @Query("Select * FROM currency")
+    fun getAllCurrencies(): Flow<List<Currency>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCurrencies(currencies: List<Currency>)
+
+    @Query("Delete FROM currency")
+    fun deleteAllCurrencies(): Int
 }
