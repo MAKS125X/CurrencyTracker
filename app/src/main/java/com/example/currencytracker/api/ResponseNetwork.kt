1b4464416df02c25e8d38e155ee69e5916b14eb4
MaksimShchelokov <MAKS125X@yandex.ru>
2023-07-10 12:43:38 +0400

    Refactored ResponseNetwork

--- app/src/main/java/com/example/currencytracker/api/ResponseNetwork.kt
+++ app/src/main/java/com/example/currencytracker/api/ResponseNetwork.kt
@@ -2,6 +2,7 @@ package com.example.currencytracker.api
 
 import com.google.gson.annotations.SerializedName
 
+
 data class ResponseNetwork(
     @SerializedName("Date")
     val date: String,
@@ -13,25 +14,5 @@ data class ResponseNetwork(
     val timestamp: String,
     @SerializedName("Valute")
     val currency: Map<String, CurrencyNetwork>
-) {
-//    fun toCurrencyModelList(): MutableList<Currency> {
-//        val list = mutableListOf<Currency>()
-//        for (valuteItem in this.valute) {
-//            list.add(
-//                Currency(
-//                    valuteItem.value.id,
-//                    valuteItem.value.numCode,
-//                    valuteItem.value.charCode,
-//                    valuteItem.value.nominal,
-//                    valuteItem.value.name,
-//                    valuteItem.value.value,
-//                    valuteItem.value.previous,
-//                    this.date
-//                )
-//            )
-//        }
-//
-//        return list
-//    }
-}
+)
 
