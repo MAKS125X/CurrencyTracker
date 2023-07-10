35d8605b3045afe0169b622158f7757cce127caf
MaksimShchelokov <MAKS125X@yandex.ru>
2023-07-10 16:05:43 +0400

    Basic viewModel for working with CurrencyRepository

--- app/src/main/java/com/example/currencytracker/ui/CurrencyViewModel.kt
+++ app/src/main/java/com/example/currencytracker/ui/CurrencyViewModel.kt
@@ -1,4 +1,18 @@
 package com.example.currencytracker.ui
 
-class CurrencyViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.example.currencytracker.repository.CurrencyRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CurrencyViewModel @Inject constructor(
+    val repository: CurrencyRepository
+) : ViewModel() {
+
+    val currencies = repository.getCurrency().asLiveData()
+
 }
