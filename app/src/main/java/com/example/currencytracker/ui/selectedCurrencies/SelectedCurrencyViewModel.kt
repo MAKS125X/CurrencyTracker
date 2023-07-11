5b1d18ce5a5d2b05c219a0d1c2152a853ce0d26b
MaksimShchelokov <MAKS125X@yandex.ru>
2023-07-11 21:56:33 +0400

    Renaming

---
+++ app/src/main/java/com/example/currencytracker/ui/selectedCurrencies/SelectedCurrencyViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.currencytracker.ui.selectedCurrencies
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.currencytracker.repository.CurrencyRepository
+import com.example.currencytracker.repository.Resource
+import com.example.currencytracker.repository.SelectedCurrency
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SelectedCurrencyViewModel @Inject constructor(
+    val repository: CurrencyRepository
+) : ViewModel() {
+
+    private val _currencies = MutableLiveData<Resource<List<SelectedCurrency>>>()
+    val currencies: LiveData<Resource<List<SelectedCurrency>>> = _currencies
+
+    fun refreshCurrencies() {
+        viewModelScope.launch {
+            repository.getCurrency().collect { result ->
+                _currencies.value = result
+            }
+        }
+    }
+}
